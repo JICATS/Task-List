2c1b943de8f968eb965bab9953c2e30388be742f
unknown <jorgeignaciocastillo@gmail.com>
2022-08-04 09:48:55 -0300

    Task-List

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { FormsModule } from '@angular/forms'
+import {RouterModule,Routes} from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+
+
+const appRoutes:Routes = [
+  {path: '', component: TasksComponent}, 
+  {path: 'about', component:AboutComponent}
+]
+
+@NgModule({
+  declarations: [ 
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TasksItemComponent,
+    AddTaskComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true})
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import {Task} from './Task'
+
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar primer modulo de Angular",
+        day: "Hacer las compras",
+        reminder: true
+
+    },
+    {
+        id: 3,
+        text: "INvestigar sobre angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true,
+    }
+]
